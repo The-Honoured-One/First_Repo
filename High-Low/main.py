5e14da7e28e5d2d661b872563f5998ed23c38abc
Ujjwal <Ujjwalagarwal602@gmail.com>
2024-03-31 22:57:56 +0530

    Set height and width and alligned the controls

--- High-Low/main.py
+++ High-Low/main.py
@@ -2,9 +2,13 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    page.window_width = 300
+    page.window_height = 300
+    page.window_resizable = False
+    page.update()
     page.title = "High-Low Game"
     display_bar = ft.Text("Welcome to high-low")
-    page.add(display_bar)
+    page.add(ft.Row([display_bar], alignment = ft.MainAxisAlignment.CENTER))
     def press_play(e):
         display_bar.value = "Select a number"
         page.remove_at(1)
@@ -13,7 +17,7 @@ def main(page: ft.Page):
         content = ft.Text("Play"),
         on_click = press_play
     )
-    page.add(play_text)
+    page.add(ft.Row([play_text], alignment = ft.MainAxisAlignment.CENTER))
     page.update()
 
     print(page.controls)
