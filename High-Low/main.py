b2d6a02e2f0c9b071d243707c2334133c029f932
Ujjwal <Ujjwalagarwal602@gmail.com>
2024-04-03 22:08:02 +0530

    Made the base of the second page and added some logic

--- High-Low/main.py
+++ High-Low/main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import random
 
 
 def main(page: ft.Page):
@@ -9,9 +10,36 @@ def main(page: ft.Page):
     page.title = "High-Low Game"
     display_bar = ft.Text("Welcome to high-low")
     page.add(ft.Row([display_bar], alignment = ft.MainAxisAlignment.CENTER))
+    def add(e):
+        main_display.value = str(int(main_display.value) + 1)
+        page.update()
+    def subtract(e):
+        main_display.value = str(int(main_display.value) - 1)
+        page.update()
+    def check (e):
+        if int(main_display.value) == target:
+            display_bar.value = "CORRECT GUESS !!"
+            page.update()
+        elif int(main_display.value) > target:
+            display_bar.value = "Go Lower, " + str(count) + " more tries"
+            page.update()        
+        else:
+            display_bar.value = "Go Higher, " + str(count) + " more tries"
+            page.update()
     def press_play(e):
         display_bar.value = "Select a number"
         page.remove_at(1)
+        page.update()
+        target = random.randint(1,9)
+        page.add(ft.Row([
+            ft.IconButton(icon = ft.icons.ADD, on_click = add),
+            main_display,
+            ft.IconButton(icon = ft.icons.REMOVE, on_click = subtract)
+        ], alignment = ft.MainAxisAlignment.CENTER))
+        page.add(ft.Row([ft.CupertinoButton(
+            content=ft.Text("GUESS", color=ft.cupertino_colors.DESTRUCTIVE_RED),
+            opacity_on_click=0.3,
+            on_click= check)], alignment = ft.MainAxisAlignment.CENTER))
         page.update()
     play_text = ft.CupertinoFilledButton(
         content = ft.Text("Play"),
@@ -19,7 +47,10 @@ def main(page: ft.Page):
     )
     page.add(ft.Row([play_text], alignment = ft.MainAxisAlignment.CENTER))
     page.update()
+    main_display = ft.Text("0")
+    count = 2
 
-    print(page.controls)
+    target = random.randint(1,9)
+
 
 ft.app(main)
